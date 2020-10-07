db16cbfa85e34d1ce00e3b11e11adb59fce8d624
Pedro Peixoto <phrfpeixoto@gmail.com>
2020-10-07 16:17:21 -0300

    Adding disclaimers and assertions to demonstrate the difference between Scrapy and Selenium

--- scrapy_selenium/spiders/demo.py
+++ scrapy_selenium/spiders/demo.py
@@ -27,9 +27,20 @@ class DemoSpider(scrapy.Spider):
         options.add_argument("--no-sandbox")
         self.driver = webdriver.Firefox(options=options, firefox_profile=profile)
 
-    def parse(self, response):
+    def parse(self, response, **kwargs):
+        """
+        Note: This is a trivial example to prove Selenium can be integrated with Crawlera and
+        executed within Scrapy Cloud. I DO NOT RECOMMEND that you use a selenium driver to
+        issue requests from within your spider's parse method.
+        """
+
+        quotes = response.css("div.quote")
+        assert len(quotes) == 0
+
         self.driver.get(response.url)
-        for quote in self.driver.find_elements_by_css_selector('div.quote'):
+        quotes = self.driver.find_elements_by_css_selector('div.quote')
+        assert len(quotes) > 0
+        for quote in quotes:
             yield {
                 'quote': quote.find_element_by_css_selector('span').text,
                 'author': quote.find_element_by_css_selector('small').text,
